626c57efc0fdfb781f9bbf559223adf0f1d99116
Bhoma-RamDaiya <bhoma05061998@gmail.com>
2023-06-17 12:20:47 +0530

    Solution is adding

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        RWOnly obj = new RWOnly();
-
-        obj.setName(" Bhoma Ram");
-        System.out.println(obj.getName());
-
-    }
-}
